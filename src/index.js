1b4d393618dedb37e899df803e0cc784b77b0487
Badreddine BENHMIDA <ben7tech@gmail.com>
2021-09-06 23:58:34 +0200

    Add function description

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 var qrcode = require('yaqrcode');
 
+/**
+ * Generate qr codes from a list of strings (a qr code for each string)
+ */
 exports.handler = async (event) => {
   const requestBody = JSON.parse(event.body);
   
