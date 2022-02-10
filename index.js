1176ec51b3db3a433677b6e542aa076a4bc838ea
dlorddd <sibatelli@gmail.com>
2022-02-10 13:28:10 +0100

    - change qr lib

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-var qrcode = require('yaqrcode');
-
 /**
  * Generate qr codes from a list of strings (a qr code for each string) :)
  */
- var qrcode = require('yaqrcode');
+import {QRCodeCanvas} from '@cheprasov/qrcode'
 
- exports.handler = async (event) => {
-   const requestBody = JSON.parse(event.body);
+exports.handler = async (event) => {
+    const requestBody = JSON.parse(event.body);
 
-   if (event.httpMethod !== 'POST' || !requestBody.items) {
-     return {
-       statusCode: 400,
-       headers: {
-         "x-custom-header" : "alpha"
-       },
-       body: 'Malfofrmed request.'
-     }
-   }
+    if (event.httpMethod !== 'POST' || !requestBody.items) {
+        return {
+            statusCode: 400,
+            headers: {
+                "x-custom-header": "alpha"
+            },
+            body: 'Malformed request.'
+        }
+    }
 
     let responseBody = requestBody.items.map(e => ({
-      item: e,
-      qr: qrcode(e), 
+        item: e,
+        qr: new QRCodeCanvas(e).toDataURL(),
     }))
 
     let response = {
-      statusCode: 200,
-      headers: {
-        "x-custom-header" : "alpha"
-      },
-      body: JSON.stringify(responseBody)
-     };
+        statusCode: 200,
+        headers: {
+            "x-custom-header": "alpha"
+        },
+        body: JSON.stringify(responseBody)
+    };
 
-     return response;
- };
+    return response;
+};
 
